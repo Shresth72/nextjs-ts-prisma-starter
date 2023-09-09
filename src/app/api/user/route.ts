2f5ce80e0468aede02666212462bc817cd6f7149
bob <97455610+Shresth72@users.noreply.github.com>
2023-09-09 18:41:03 +0530

    user route with caching

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,22 @@
 import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
 
 export async function GET() {
   try {
-    const user = await db.user.findFirst();
-    console.log(user);
+    const cachedValue = await redis.get("User Name");
+    if (cachedValue) {
+      return new Response(cachedValue);
+    }
 
-    return new Response("User found");
+    const user = await db.user.findFirst();
+
+    if (!user) return new Response("Error");
+
+    await redis.set("User Name", JSON.stringify(user));
+
+    return new Response(JSON.stringify(user));
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return new Response("User not found!");
   }
 }
